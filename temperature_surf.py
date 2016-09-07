6766dfa2f51a60b09ad55e4e7d2bbe4464ec137b
van_mit <ivan.mitskovets@gmail.com>
2016-09-07 17:14:51 +0300

    learn how talk with pslq

--- temperature_surf.py
+++ temperature_surf.py
@@ -1,5 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import postgresql.exceptions
 import postgresql.driver as pg_driver
 
-db = pg_driver.connect(user='postgres', password='postgres', host='localhost', port=5432, database='family')
+try:
+    db = pg_driver.connect(user='postgres', password='postgres', host='localhost', port=5432, database='family')
+    print("Connect to postgres successfully!")
+except postgresql.exceptions.ClientCannotConnectError:
+    print('Cannot connect! Check your internet connection and psql server status.')
+    exit(-1)
+
 ps = db.prepare("SELECT now()")
 print(ps())
