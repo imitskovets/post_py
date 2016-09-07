3286d464ed88c5cee0cfbf676bdc643e99168740
van_mit <ivan.mitskovets@gmail.com>
2016-09-07 17:18:21 +0300

    learn how talk with pslq

--- temperature_surf.py
+++ temperature_surf.py
@@ -2,9 +2,14 @@
 # -*- coding: utf-8 -*-
 import postgresql.exceptions
 import postgresql.driver as pg_driver
+user = 'postgres'
+password = 'postgres'
+host = 'localhost'
+db_family = 'family'
+db_measurements = 'measurements'
 
 try:
-    db = pg_driver.connect(user='postgres', password='postgres', host='localhost', port=5432, database='family')
+    db = pg_driver.connect(user=user, password=password, host=host, port=5432, database=db_family)
     print("Connect to postgres successfully!")
 except postgresql.exceptions.ClientCannotConnectError:
     print('Cannot connect! Check your internet connection and psql server status.')
