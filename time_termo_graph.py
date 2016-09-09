23a24a4802dd17ebef230be79701bda893f0ccb8
van_mit <ivan.mitskovets@gmail.com>
2016-09-09 10:32:08 +0300

    draw b-spline from data in psql, need to correct axe x and colours

--- time_termo_graph.py
+++ time_termo_graph.py
@@ -1,8 +1,44 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+
+import numpy as np
 import postgresql.exceptions
 import postgresql.driver as pg_driver
+from scipy import interpolate
 import matplotlib.pyplot as plt
+
+
+def draw_through_points(x_plt, y_plt, plt_def, n):
+    plt_def.figure()
+    tck, u = interpolate.splprep([x_plt, y_plt], k=3, s=0)
+    u = np.linspace(0, 1, num=n, endpoint=True)
+    out = interpolate.splev(u, tck)
+    plt_def.plot(x_plt, y_plt, 'ro', out[0], out[1], 'b')
+    plt_def.legend(['Points', 'Interpolated B-spline', 'True'], loc='best')
+    plt_def.axis([min(x_plt) - 1, max(x_plt) + 1, min(y_plt) - 1, max(y_plt) + 1])
+    plt_def.title('Temperature in object')
+    plt_def.grid(True)
+    plt_def.show()
+
+
+def draw_beautiful(x_plt, y_plt, plt_def):
+    plt_def.figure()
+    l = len(x_plt)
+    t = np.linspace(0, 1, l - 2, endpoint=True)
+    t = np.append([0, 0, 0], t)
+    t = np.append(t, [1, 1, 1])
+    tck = [t, [x_plt, y_plt], 3]
+    u3 = np.linspace(0, 1, (max(l * 2, 100)), endpoint=True)
+    out = interpolate.splev(u3, tck)
+    plt_def.plot(x_plt, y_plt, 'ro', label='Measured points', marker='o', markerfacecolor='green')
+    plt_def.plot(out[0], out[1], 'b', linewidth=2.0, label='B-spline curve')
+    plt_def.legend(loc='best')
+    plt_def.xlabel('Time (s)')
+    plt_def.ylabel('Temperature (ºC)')
+    plt_def.axis([min(x_plt) - 1, max(x_plt) + 1, min(y_plt) - 1, max(y_plt) + 1])
+    plt_def.title('Temperature in object')
+    plt_def.grid(True)
+    plt_def.show()
 
 
 user = 'postgres'
@@ -22,7 +58,8 @@ except postgresql.exceptions.ClientCannotConnectError:
 
 ps = db.prepare('select * from ' + table_name + ' where time > ' + str(start_time) + ' ;')
 points = ps()
-print(points)
-#plt.plot(points)
-#plt.grid(True)
-#plt.show()
+draw_data = np.array(points)
+x = draw_data[:, 0]
+y = draw_data[:, 1]
+# draw_through_points(x, y, plt, n=len(x))
+draw_beautiful(x, y, plt)
